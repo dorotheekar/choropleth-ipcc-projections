532a462a018807288da94d5d407c3f51d73262f9
Dorothée <ka.dorothee@gmail.com>
2022-06-16 16:01:22 +0200

    Adding locations.csv

--- input.py
+++ input.py
@@ -105,85 +105,53 @@ else :
         filename = f'./data/{variable_name}/{period}_projections/*.nc'
         # Located files where we will get the data
 
+# Computing number of years, will be necessary to compute year average
 
 years_number = int(f'{end_date[:4]}') - int(f'{start_date[:4]}')
 
 print(f"Your map will be displayed on {years_number} year(s).")
 
 ##################
-# Custom location points
-
-# Initialisation of those variables. Those won't be needed if the user decides to have no marker on the map
-list_of_latitudes = []
-list_of_longitudes = []
-color_list =[]
-text_marker =[]
-marker_size = []
-custom=[]
-#
-
 # Asking user if he wants to have location markers
 while True :
     location_marker = input(">>> Would you like to display location markers on your map ? (Y/N) = ")
 
     if location_marker == 'N' : # user doesn't want any location marker
+
+        list_of_latitudes = []
+        list_of_longitudes = []
+        color_list = []
+        text_marker = []
+        marker_size  = []
+
         print("Your choice has been saved.")
         
         break
 
     if location_marker == "Y" : # user wants to display locations markers
-        custom = input(">>> Would you like to custom your location markers ? (Y/N) = ")
+        locations_csv = pd.read_csv('locations.csv', sep = ';')
+
+        list_of_latitudes = locations_csv['list_of_latitudes'].to_list()
+        list_of_longitudes = locations_csv['list_of_longitudes'].to_list()
+        color_list = locations_csv['color_list'].to_list()
+        text_marker = locations_csv['text_marker'].to_list()
+        marker_size = locations_csv['marker_size'].to_list()
         
-        if custom == "Y" : # user want to custom location markers
+        break
 
-            n = int(input(">>> How many points do you want to display ? "))
-
-            for i in range(0, n):
-                city = input('>> Enter location name = ') # give the city name on the marker
-                text_marker.append(city)
-
-                lat = float(input('>> Enter the corresponding latitude = ')) 
-                list_of_latitudes.append(lat)
-
-                lon = float(input('>> Enter the corresponding longitude = '))
-                list_of_longitudes.append(lon)
-
-                color_pt = input('>> Enter the corresponding color (red or yellow) = ' ) # any other color could be chosen
-                color_list.append(color_pt)
-
-
-            marker_size = int(input('>> Specify the marker size (recommended : 12) = '))
-            print("Your choice has been saved.")
-            break
-
-        if custom  == "N" :
-            print('>> Markers will be: Rouen, Lyon, Paris, Porto, Milan.')
-
-            list_of_latitudes = [48.879349, 45.441975, 48.772090, 41.264996, 45.457721 ]
-            list_of_longitudes = [2.196998,  6.382717, 2.549041, -8.595143, 9.602173]
-            color_list = ['red', 'red', 'yellow', 'yellow', 'yellow']
-            marker_size = 12
-            text_marker = ['Rouen', 'Lyon', 'Paris', 'Porto', 'Milan']
-
-            
-            break
-
-        else :
-            print("Error: Please answer Y or N at the previous question.")
-            
     else : 
         print("Error: Please answer Y or N at the previous question.")
         
+        
 ##################
 # Final user choice of custom map 
-
 
 user_choice = choropleth_function(
 
     variable_input, variable_name, unit, threshold,
     start_date, end_date, years_number,
     text_description, period, legend, filename,
-    text_marker, location_marker, custom, list_of_latitudes, list_of_longitudes, color_list, marker_size,
+    location_marker, text_marker, list_of_latitudes, list_of_longitudes, color_list, marker_size,
     color_continuous_scale
 
     )
